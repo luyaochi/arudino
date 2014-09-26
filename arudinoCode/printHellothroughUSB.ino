aeff9ffe2fa9c1259c4f4995d1879a03c953ed70
ab123ab456g <ab123ab456g@gmail.com>
2014-09-27 06:27:40 +0800

    init

---
+++ arudinoCode/printHellothroughUSB.ino
@@ -0,0 +1,33 @@
+//source from imacnoob.blogspot.tw/2012/11/pythonarduino.htmls
+// set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);      
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);     
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {     
+    // turn LED on:    
+    Serial.println("Hello");
+    digitalWrite(ledPin, HIGH);  
+    delay(300);
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
+}
